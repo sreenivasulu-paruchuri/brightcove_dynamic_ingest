13a4fc24156637a2b4959d493effe5e80f4a77d9
srijan-ravi <rksy.ravi@gmail.com>
2021-03-07 00:22:13 +0530

    Running functionality, but need modification to it.

---
+++ js/brightcove-dynamic-ingest.js
@@ -0,0 +1,275 @@
+'use strict';
+
+(function ($, Drupal) {
+  Drupal.behaviors.uploadBcFiles = {
+    attach: function (context, settings) {
+      var $baseUrl = settings.path.baseUrl;
+      var $form = 'form#bc-dynamic-ingest-form';
+
+      function videoProgress($display, $progress = null) {
+        var $mainId = 'brightcove-dynamic-ingest-loader';
+        var $innerId = 'brightcove-dynamic-ingest-inner';
+        var $progressId = 'video-progress';
+        var $spinner = '<div class="overlay-spinner"><span class="spinner"></span></div>';
+        var $content = '<div class="loader-content">Initiating video upload<br>Please wait...</div>';
+        var $selector = $('html > body');
+        if ($selector.find('#' + $mainId).length == 0) {
+          $selector.append('<div style="display: none;" id="' + $mainId + '"><div id="' + $innerId + '">' + $spinner + $content + '</div></div>');
+        }
+
+        if ($selector.find('div#' + $mainId).length > 0 && $display === true) {
+          $selector.css('overflow', 'hidden');
+          $selector.find('#' + $mainId).show();
+        }
+
+        if ($progress) {
+          var $finalMessage = ($progress == 100) ? '<br>Finalizing request' : '';
+          $('#' + $mainId + ' #' + $innerId + ' .loader-content').replaceWith('<div class="loader-content">Video uploaded...<span id="' + $progressId + '">('+ $progress + '%)</span>' + $finalMessage + '</div>');
+        }
+      }
+
+      function getFieldsValue($name) {
+        switch ($name) {
+          case 'title':
+            return $($form).find("[name='title']").val();
+          case 'desc':
+            return $($form).find("[name='description']").val();
+          case 'video':
+            return $($form).find("[name='files[video]']").get()[0].files[0];
+          case 'image':
+            return $($form).find("[name='thumbnail[fids]']").val();
+          case 'watermark':
+            return $($form).find("[name='watermark']:checked").val();
+        }
+      }
+
+      function getTags() {
+        var $tags = settings.brightcoveDynamicIngest.noWatermark.tags;
+        if (typeof getFieldsValue('watermark') !== 'undefined') {
+          $tags = settings.brightcoveDynamicIngest.watermark.tags;
+        }
+        return $tags;
+      }
+
+      function getApiUrl($urltype) {
+        var $accountId = settings.brightcoveDynamicIngest.noWatermark.accountId;
+        if (typeof getFieldsValue('watermark') !== 'undefined') {
+          $accountId = settings.brightcoveDynamicIngest.watermark.accountId;
+        }
+        switch ($urltype) {
+          case 'cms':
+            return 'https://cms.api.brightcove.com/v1/accounts/' + $accountId + '/videos/';
+          case 'ingest':
+            return 'https://ingest.api.brightcove.com/v1/accounts/' + $accountId + '/videos/';
+        }
+      }
+
+      /**
+       * @param Object
+       * @returns boolean
+       */
+      function isJSON ($text) {
+        if (typeof $text !== 'string') {
+          $text = JSON.stringify($text);
+        }
+
+        try {
+          JSON.parse($text);
+          return true;
+        } catch (e) {
+          return false;
+        }
+      }
+
+      function checkRequired() {
+        var $check = false;
+        if (getFieldsValue('title').length === 0 || typeof getFieldsValue('video') === 'undefined') {
+          $check = true;
+        }
+        return $check;
+      }
+
+      function finalizeUpload($status, $location, $message = null) {
+        $.ajax({
+          url: $baseUrl + 'brightcove_dynamic_ingest/finalize-upload',
+          method: 'POST',
+          cache: false,
+          async: false,
+          data: {'status': $status, 'location': $location, 'message': $message},
+          success: function ($url) {
+            location.replace($url);
+          },
+          error: function ($error) {
+            location.reload();
+          }
+        });
+      }
+
+      function getAuthToken() {
+        var $functionName = 'getAuthToken';
+        var $result = false;
+        $.ajax({
+          url: $baseUrl + 'brightcove_dynamic_ingest/get-token',
+          async: false,
+          success: function (data) {
+            if (isJSON(data) === false) {
+              finalizeUpload('error', $functionName, data);
+            }
+            $result = $.parseJSON(data);
+            if ($result.error) {
+              finalizeUpload('error', $functionName, $result.error_description);
+            }
+          },
+          error: function($error){
+            finalizeUpload('error', $functionName, $error.responseText);
+          }
+        });
+        return $result;
+      }
+
+      function ingestUrl($video, $uploadUrls, $fileUrl = null) {
+        var $functionName = 'ingestUrl';
+        var $tokens = getAuthToken();
+        var $pushData = {'master': {'url': $uploadUrls.api_request_url}};
+        if ($fileUrl) {
+          $pushData.poster = {'url': $fileUrl};
+          $pushData.thumbnail = {'url': $fileUrl};
+          $pushData['capture-images'] = false;
+        }
+        $.ajax({
+          url: getApiUrl('ingest') + $video.id + '/ingest-requests',
+          method: 'POST',
+          cache: false,
+          async: false,
+          data: JSON.stringify($pushData),
+          headers: {
+            'Authorization': $tokens.token_type + ' ' + $tokens.access_token,
+            'Content-Type': 'application/json',
+          },
+          success: function (result) {
+            finalizeUpload('success', $functionName);
+          },
+          error: function($error){
+            finalizeUpload('error', $functionName, $error.responseText);
+          }
+        });
+      }
+
+      function getThumbnailUrl($video, $uploadUrls, $fid) {
+        var $functionName = 'getThumbnailUrl';
+        $.ajax({
+          url: $baseUrl + 'brightcove_dynamic_ingest/get-thumbnail',
+          method: 'POST',
+          cache: false,
+          async: false,
+          data: {'fid': $fid, 'location': $functionName},
+          success: function ($fileUrl) {
+            ingestUrl($video, $uploadUrls, $fileUrl);
+          },
+          error: function ($error) {
+            finalizeUpload('error', $functionName, $error.responseText);
+          }
+        });
+      }
+
+      function uploadVideo($video, $uploadUrls) {
+        var $functionName = 'uploadVideo';
+        AWS.config.update({
+          accessKeyId: $uploadUrls.access_key_id,
+          secretAccessKey: $uploadUrls.secret_access_key,
+          sessionToken: $uploadUrls.session_token,
+        });
+        var upload = new AWS.S3.ManagedUpload({
+          params: {
+            Bucket: $uploadUrls.bucket,
+            Key: $uploadUrls.object_key,
+            Body: getFieldsValue('video')
+          }
+        });
+
+        upload.on('httpUploadProgress', function (progress) {
+          setTimeout(function(){
+            videoProgress(true, parseInt((progress.loaded * 100) / progress.total));
+          }, 1);
+        });
+
+        var promise = upload.promise();
+        promise.then(
+          function(data) {
+            var $imageFid = getFieldsValue('image');
+            if (typeof $imageFid !== 'undefined' && $imageFid.length > 0) {
+              getThumbnailUrl($video, $uploadUrls, $imageFid);
+            }
+            else {
+              ingestUrl($video, $uploadUrls);
+            }
+          },
+          function($error) {
+            finalizeUpload('error', $functionName, $error.message);
+          }
+        );
+      }
+
+      function getUploadUrls($tokens, $video) {
+        var $functionName = 'getUploadUrls';
+        $.ajax({
+          url: getApiUrl('cms') + $video.id + '/upload-urls/' + getFieldsValue('video').name,
+          method: 'GET',
+          cache: false,
+          async: false,
+          headers: {
+            'Authorization': $tokens.token_type + ' ' + $tokens.access_token,
+          },
+          success: function ($uploadUrls) {
+            uploadVideo($video, $uploadUrls);
+          },
+          error: function($error){
+            finalizeUpload('error', $functionName, $error.responseText);
+          }
+        });
+      }
+
+      function createVideo() {
+        var $functionName = 'createVideo';
+        var $pushData = {'name': getFieldsValue('title'), 'description': getFieldsValue('desc'), 'tags': getTags()};
+        var $tokens = getAuthToken();
+        $.ajax({
+          url: getApiUrl('cms'),
+          method: 'POST',
+          cache: false,
+          async: false,
+          data: JSON.stringify($pushData),
+          headers: {
+            'Authorization': $tokens.token_type + ' ' + $tokens.access_token,
+            'Content-Type': 'application/json',
+          },
+          success: function ($video) {
+            getUploadUrls($tokens, $video);
+          },
+          error: function($error){
+            finalizeUpload('error', $functionName, $error.responseText);
+          }
+        });
+      }
+
+      videoProgress(false);
+      // Form submit.
+      $($form).once().on('submit', function (e){
+        e.preventDefault();
+        if (checkRequired() === false) {
+          var $fileSize = getFieldsValue('video').size;
+          if ($fileSize > (1024*1024*1024*2)) {
+            return alert('Video should not be greater than 2GB');
+          }
+          videoProgress(true);
+          setTimeout(function (){
+            createVideo();
+          }, 2);
+        }
+        else {
+          alert('Please make sure to fill-up all the required fields');
+        }
+      });
+    }
+  };
+})(jQuery, Drupal);
---
+++ src/Controller/BcDynamicIngestController.php
@@ -0,0 +1,187 @@
+<?php
+
+namespace Drupal\brightcove_dynamic_ingest\Controller;
+
+use Drupal\Core\Controller\ControllerBase;
+use Drupal\Core\Url;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+/**
+ * Controller for the Brightcove access token.
+ */
+class BcDynamicIngestController extends ControllerBase {
+
+  /**
+   * Brightcove uploading log channel.
+   */
+  const LOG_CHANNEL = 'brightcove video upload';
+
+  /**
+   * Symfony\Component\HttpFoundation\RequestStack definition.
+   *
+   * @var \Symfony\Component\HttpFoundation\RequestStack
+   */
+  protected $requestStack;
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container) {
+    $instance = parent::create($container);
+    $instance->requestStack = $container->get('request_stack');
+    return $instance;
+  }
+
+  /**
+   * Returns the access token of Brightcove.
+   */
+  public function getOauthToken() {
+    $client = $this->getBrightcoveClientKeys();
+    // Set up request for access token.
+    $data          = [];
+    $client_id     = $client["clientId"];
+    $client_secret = $client["secretKey"];
+    $auth_string   = "{$client_id}:{$client_secret}";
+    $request       = "https://oauth.brightcove.com/v4/access_token?grant_type=client_credentials";
+    $ch            = curl_init($request);
+    curl_setopt_array($ch, [
+      CURLOPT_POST           => TRUE,
+      CURLOPT_RETURNTRANSFER => TRUE,
+      CURLOPT_SSL_VERIFYPEER => FALSE,
+      CURLOPT_USERPWD        => $auth_string,
+      CURLOPT_HTTPHEADER     => [
+        'Content-type: application/x-www-form-urlencoded',
+      ],
+      CURLOPT_POSTFIELDS => $data,
+    ]);
+    $response = curl_exec($ch);
+    curl_close($ch);
+
+    // Check for errors.
+    if ($response === FALSE) {
+      echo 'An error occurred';
+    }
+    else {
+      echo $response;
+    }
+    exit;
+  }
+
+  /**
+   * Logs and display message and redirect to MGC form.
+   */
+  public function finalizeUpload() {
+    $request = $this->requestStack->getCurrentRequest()->request;
+    if (!empty($request) && !empty($request->get('status'))) {
+      if ($request->get('status') == 'success') {
+        $this->getLogger(self::LOG_CHANNEL)->info($this->t('Video has been uploaded successfully'));
+        $this->messenger()->addStatus($this->t('Video has been uploaded successfully'));
+      }
+      else {
+        $this->getLogger(self::LOG_CHANNEL)->error($this->t('Function(API) called: @location<br>@log', [
+          '@location' => $request->get('location'),
+          '@log' => $request->get('message'),
+        ]));
+        $this->messenger()->addError($this->t('An error occurred while uploading video. Please check logs or try again!'));
+      }
+    }
+    echo Url::fromRoute('brightcove_dynamic_ingest.add_form')->setAbsolute()->toString();
+    exit;
+  }
+
+  /**
+   * Brightcove clients.
+   *
+   * @param bool $watermark
+   *   Watermark status for brightcove api.
+   *
+   * @return array
+   *   Array of brightcove clients.
+   */
+  public function getBrightcoveClientKeys($watermark = FALSE) {
+    $client = [];
+    $brightcoveConfig = $this->entityTypeManager()->getStorage('brightcove_api_client');
+    $ids = $brightcoveConfig->getQuery('brightcove_api_client')->execute();
+    if (in_array('watermark', $ids) && !$watermark) {
+      unset($ids['watermark']);
+    }
+    elseif (in_array('watermark', $ids) && $watermark) {
+      $ids = ['watermark'];
+    }
+    $list = $brightcoveConfig->loadMultiple($ids);
+    foreach ($list as $entity) {
+      $client['clientId'] = $entity->get('client_id');
+      $client['secretKey'] = $entity->get('secret_key');
+      $client['accountId'] = $entity->get('account_id');
+    }
+    return $client;
+  }
+
+  /**
+   * Tags to push to Brightcove.
+   *
+   * @param bool $watermark
+   *   Watermark status for brightcove api.
+   *
+   * @return array|false|string[]
+   *   Tags to attach with.
+   */
+  public function getPushTags($watermark = FALSE) {
+    $tags = [];
+    $config = $this->config('mc_brightcove.settings');
+    if (!empty($config->get('tags_to_push')) && !$watermark) {
+      $tags = explode(',', $config->get('tags_to_push'));
+    }
+    if (!empty($config->get('watermark_tags_to_push')) && $watermark) {
+      $tags = explode(',', $config->get('watermark_tags_to_push'));
+    }
+    if (!empty($config->get('youtube_label'))) {
+      $tags[] = $config->get('youtube_label');
+    }
+    return $tags;
+  }
+
+  /**
+   * Get upload location on cloudinary.
+   *
+   * @return string
+   *   Upload location.
+   */
+  public function getUploadLocation() {
+    $target_dir = 'cloudinary://mediacorp/one-cms/images/';
+    $image_media_entity = $this->entityFieldManager->getFieldDefinitions('media', 'image');
+    if (!empty($image_media_entity) && !empty($image_media_entity['field_media_image'])) {
+      $token_replaced = $this->token->replace($image_media_entity['field_media_image']->getSetting('file_directory'));
+      $target_dir = 'cloudinary://' . trim($token_replaced, '/') . '/';
+    }
+    return $target_dir;
+  }
+
+  /**
+   * Get full path of image.
+   */
+  public function getThumbnail() {
+    $url = NULL;
+    $request = $this->requestStack->getCurrentRequest()->request;
+    if (!empty($request) && !empty($request->get('fid'))) {
+      $fid = $request->get('fid');
+      if (is_numeric($fid) && $file = $this->entityTypeManager()->getStorage('file')->load($fid)) {
+        $url = file_create_url($file->getFileUri());
+        $this->getLogger(self::LOG_CHANNEL)->info($this->t('Function(API) called: @location<br>Image url: @fid (@url)', [
+          '@location' => $request->get('location'),
+          '@fid' => $fid,
+          '@url' => $url,
+        ]));
+      }
+      else {
+        $this->getLogger(self::LOG_CHANNEL)->error($this->t('Function(API) called: @location<br>Not a valid fid: @fid', [
+          '@location' => $request->get('location'),
+          '@fid' => $fid,
+        ]));
+      }
+    }
+    echo $url;
+    exit;
+  }
+
+}
